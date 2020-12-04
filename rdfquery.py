40cedff4ae69b5fa2f69252b9e92807f454939d3
marco <marcootomasi@gmail.com>
2020-12-04 19:18:16 +0100

    Aggiunte query più generiche e gestione di alcuni sinonimi delle parole più usate

--- rdfquery.py
+++ rdfquery.py
@@ -2,14 +2,14 @@ from contextlib import contextmanager
 from ctypes import CFUNCTYPE, c_char_p, c_int, cdll
 import rdflib
 from gtts import gTTS
-from googletrans import Translator
-from googletrans import constants
 import playsound
 import speech_recognition as sr
-
 import contextlib
 import os
 import sys
+
+'''Funzione che elimina gli errori sul terminale (bug della libreria gTTS)'''
+
 
 @contextlib.contextmanager
 def ignore_stderr():
@@ -25,9 +25,7 @@ def ignore_stderr():
         os.close(old_stderr)
 
 
-translator = Translator(service_urls=constants.DEFAULT_SERVICE_URLS)
-g = rdflib.Graph()
-g.parse("db.rdf")
+'''Funzione che riproduce un audio che contiene la stringa text'''
 
 
 def speak(text):
@@ -40,14 +38,16 @@ def speak(text):
         os.remove(filename)
 
 
+'''Funzione che ascolta la frase che dice l'utente e la trasforma in stringa'''
+
+
 def getAudio():
     with noalsaerr():
         ignore_stderr()
         r = sr.Recognizer()
         with sr.Microphone() as source:
-            audio = r.listen(source)
+            audio = r.listen(source, 5)
             said = ""
-
             try:
                 said = r.recognize_google(audio, language='en-US')
                 print(said)
@@ -56,118 +56,136 @@ def getAudio():
         return said
 
 
-# Questa funzione restituisce il numero di pubblicazioni relative alla nazione che riceve come parametro
-def pubblicazioni_per_nazione(stringname):
-    sparql = '''PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+# Funzione che recupera la lista delle nazioni
+def listaPerTipo(tipo):
+    sparql = f'''PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+                    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
+                    SELECT ?json_data_name
+                    WHERE 
+                    {{
+                        ?json_data_primaryId db:json_data_name ?json_data_name .
+                        ?json_data_primaryId db:json_data_type ?json_data_type .
+                        ?json_data_primaryId db:json_data_num_publications ?json_data_num_publications .
+                        FILTER (lcase(str(?json_data_type)) = "{tipo.lower()}")
+                    }}       
+                    '''
+    response = g.query(sparql)
+    total = []
+    for row in response:
+        total.append(row.asdict()['json_data_name'].toPython())
+    total = list(dict.fromkeys(total))
+    total = [each_string.lower() for each_string in total]
+    total.sort()
+    if '' in total:
+        total.remove('')
+    return total
+
+def informazioniTotali(nome):
+    tipo = ''
+    citazioni = ''
+    pubblicazioni = ''
+    sparql = f'''PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
                 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
                 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
-                SELECT ?json_data_num_publications
+                SELECT ?json_data_name ?json_data_type (SUM(xsd:integer(?json_data_num_citations)) AS ?totCit) (SUM(xsd:integer(?json_data_num_publications)) AS ?totPub)
                 WHERE 
-                {
+                {{ 
                     ?json_data_primaryId db:json_data_name ?json_data_name .
                     ?json_data_primaryId db:json_data_type ?json_data_type .
                     ?json_data_primaryId db:json_data_num_publications ?json_data_num_publications .
-                    FILTER (regex(?json_data_type, "country")) .
-                    FILTER (regex(?json_data_name, "''' + stringname + '''"))
-                }       
+                    ?json_data_primaryId db:json_data_num_citations ?json_data_num_citations .
+                    FILTER (lcase(str(?json_data_name)) = "{nome.lower()}")
+                }}
+                GROUP BY ?json_data_name'''
+    response = g.query(sparql)
+    for row in response:
+        nome = (row.asdict()['json_data_name'].toPython())
+        tipo = (row.asdict()['json_data_type'].toPython())
+        citazioni = (row.asdict()['totCit'].toPython())
+        pubblicazioni = (row.asdict()['totPub'].toPython())
+    article = 'a'
+    if tipo == 'author' or tipo == 'institution':
+        article = 'an'
+    frasedadire = f'''{nome} is {article} {tipo}. The number of citations is {citazioni} and the number of publications is {pubblicazioni}'''
+    print(frasedadire)
+    speak(frasedadire)
+
+# Questa funzione restituisce il numero di pubblicazioni relative alla nazione che riceve come parametro
+def pubblicazioni_per_nome(tipo, dato, nome):
+    sparql = f'''PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
+                SELECT ?json_data_num_{dato}
+                WHERE 
+                {{
+                    ?json_data_primaryId db:json_data_name ?json_data_name .
+                    ?json_data_primaryId db:json_data_type ?json_data_type .
+                    ?json_data_primaryId db:json_data_num_{dato} ?json_data_num_{dato} .
+                    FILTER (regex(?json_data_type, "{tipo}")) .
+                    FILTER (regex(?json_data_name, "{nome}"))
+                }}       
                 '''
     response = g.query(sparql)
     total = 0
     for row in response:
-        total += int(row.asdict()['json_data_num_publications'].toPython())
+        total += int(row.asdict()[f'''json_data_num_{dato}'''].toPython())
 
     if total != 0:
-        return 'The number of publications in ' + stringname.capitalize() + ' is ' + str(total)
+        frasedadire = f'''The number of {dato} of the {tipo} {nome} is {str(total)}'''
     else:
-        return 'The question returned no results'
+        frasedadire = 'The question returned no results'
+    print(frasedadire)
+    speak(frasedadire)
 
-
-def maggior_pubblicazioni(stringtype):
-
-    sparql = '''PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+def maggior_pubblicazioni(tipo, dato, ordine):
+    if ordine == 'DESC':
+        o = "most"
+    else:
+        o = 'fewest'
+    sparql = f'''PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
                 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
                 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
-                SELECT ?json_data_name ?json_data_type (SUM(xsd:integer(?json_data_num_publications)) AS ?tot)
+                SELECT ?json_data_name ?json_data_type (SUM(xsd:integer(?json_data_num_{dato})) AS ?tot)
                 WHERE 
-                {
+                {{
                     ?json_data_primaryId db:json_data_name ?json_data_name .
                     ?json_data_primaryId db:json_data_type ?json_data_type .
                     ?json_data_primaryId db:json_data_num_publications ?json_data_num_publications .
                     ?json_data_primaryId db:json_data_num_citations ?json_data_num_citations  
-                    FILTER( regex(?json_data_type, "''' + stringtype + '''") )
-                }
+                    FILTER( regex(?json_data_type, "{tipo}") )
+                }}
                 GROUP BY ?json_data_name
-                ORDER BY DESC(?tot) LIMIT 1                       
+                ORDER BY {ordine} (?tot) LIMIT 1                       
             '''
-    fraseDaDire = "Sorry, I can't answer this question"
+    #fraseDaDire = "Sorry, I can't answer this question"
     response = g.query(sparql)
     string = ''
     number = ''
     for row in response:
         string = row.asdict()['json_data_name'].toPython()
         number = row.asdict()['tot'].toPython()
-    if stringtype.capitalize() == 'Author':
-        fraseDaDire = "The author who has published the most is " + string + " with " + str(number) + " publications"
-    if stringtype.capitalize() == 'Conference':
-        fraseDaDire = "The conference with the most publications is " + string + " with " + str(number) + " publications"
-    if stringtype.capitalize() == 'Country':
-        fraseDaDire = "The country with the most publications is " + string + " with " + str(number) + " publications"
-    if stringtype.capitalize() == 'Institution':
-        fraseDaDire = "The institution with the most publications is" + string + " with " + str(number) + " publications"
-    if stringtype.capitalize() == 'Journal':
-        fraseDaDire = "The journal with the most publications is " + string + " with " + str(number) + " publications"
-    if stringtype.capitalize() == 'Topic':
-        fraseDaDire = "The topic with the most publications is " + string + " with " + str(number) + " publications"
-    speak(fraseDaDire)
-
-
-def minor_pubblicazioni(stringtype):
-
-    sparql = '''PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
-                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
-                SELECT ?json_data_name ?json_data_type (SUM(xsd:integer(?json_data_num_publications)) AS ?tot)
-                WHERE 
-                {
-                    ?json_data_primaryId db:json_data_name ?json_data_name .
-                    ?json_data_primaryId db:json_data_type ?json_data_type .
-                    ?json_data_primaryId db:json_data_num_publications ?json_data_num_publications .
-                    ?json_data_primaryId db:json_data_num_citations ?json_data_num_citations  
-                    FILTER( regex(?json_data_type, "''' + stringtype + '''") )
-                }
-                GROUP BY ?json_data_name
-                ORDER BY ASC(?tot) LIMIT 1                       
-            '''
-    response = g.query(sparql)
-    fraseDaDire = "Sorry, I can't answer this question"
-    string = ''
-    number = ''
-    for row in response:
-        string = row.asdict()['json_data_name'].toPython()
-        number = row.asdict()['tot'].toPython()
-    if stringtype.capitalize() == 'Author':
-        fraseDaDire = "The least published author is " + string + " with " + str(number) + " publications"
-    if stringtype.capitalize() == 'Conference':
-        fraseDaDire = "The conference with the fewest publications is " + string + " with " + str(number) + " publications"
-    if stringtype.capitalize() == 'Country':
-        fraseDaDire = "The country with the fewest publications is " + string + " with " + str(number) + " publications"
-    if stringtype.capitalize() == 'Institution':
-        fraseDaDire = "The institution with the fewest publications is " + string + " with " + str(number) + " publications"
-    if stringtype.capitalize() == 'Journal':
-        fraseDaDire = "The least published magazine is " + string + " with " + str(number) + " publications"
-    if stringtype.capitalize() == 'Topic':
-        fraseDaDire = "The topic with fewer publications is " + string + " with " + str(number) + " publications"
+    fraseDaDire = f'''The {tipo} with the {o} {dato} is {string} with {str(number)} {dato}'''
+    print(fraseDaDire)
     speak(fraseDaDire)
 
 
 # ---------------------Inizio Script------------------------------------
 
+'''Parse del json in rdf'''
+g = rdflib.Graph()
+g.parse("db.rdf")
+
+# Funzioni per la rimozione degli errori a terminale causati da un bug sulla libreria gTTS
 ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+
 
 def py_error_handler(filename, line, function, err, fmt):
     pass
 
+
 c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+
 
 @contextmanager
 def noalsaerr():
@@ -176,34 +194,61 @@ def noalsaerr():
     yield
     asound.snd_lib_error_set_handler(None)
 
+
+# Inizializzazione dell'assistente Scholarly Data
+# Array contenente le categorie contenute nell'rdf
+listaTipi = ["author", "conference", "country", "institution", "journal", "topic", "institute"]
+# Saluto dell'assistente
+print("Hi, I'm Scholarly Data! Give me a question!")
 speak("Hi, I'm Scholarly Data! Give me a question!")
 risp = 'yes'
 first = True
 while risp.__contains__('yes'):
+    success = False
     if not first:
+        print("Ok, give me a question!")
         speak("Ok, give me a question!")
     first = False
-    query = getAudio()
+    # query = getAudio().lower()
+    query = input()
+    for t in listaTipi:
+        for parola in listaPerTipo(t):
+            if query.lower().__contains__(parola):
+                if query.lower().__contains__('publications'):
+                    pubblicazioni_per_nome(t, 'publications', parola)
+                    success = True
+                else:
+                    if query.lower().__contains__('citations'):
+                        pubblicazioni_per_nome(t, 'citations', parola)
+                        success = True
+                    else:
+                        if query.lower() == parola:
+                            informazioniTotali(parola)
+                            success = True
+            if success:
+                break
+        if query.lower().__contains__(t):
+            if query.__contains__('more') or query.lower().__contains__('major') or query.lower().__contains__('most'):
+                if query.lower().__contains__('publications'):
+                   maggior_pubblicazioni(t, 'publications', 'DESC')
+                   success = True
+                if query.lower().__contains__('citations'):
+                   maggior_pubblicazioni(t, 'citations', 'DESC')
+                   success = True
+            if query.lower().__contains__('less') or query.lower().__contains__('fewer') or query.lower().__contains__('least'):
+                if query.__contains__('publications'):
+                    maggior_pubblicazioni(t, 'publications', 'ASC')
+                    success = True
+                if query.lower().__contains__('citations'):
+                    maggior_pubblicazioni(t, 'citations', 'ASC')
+                    success = True
+    if not success:
+        print("Sorry, I can't answer this question")
+        speak("Sorry, I can't answer this question")
 
-    if query.__contains__('how many publications have there been in '):
-        split = query.split(' ')
-        string = split[7]
-        if len(split) > 8:
-            string = string + ' ' + split[8]
-        result = pubblicazioni_per_nazione(string)
-        print(result)
-        speak(result)
-    else:
-        if query.__contains__('has published more'):
-            '''Query: which ...'''
-            maggior_pubblicazioni(query.split(' ')[1])
-        else:
-            if query.__contains__('has published less'):
-                '''Query: which ...'''
-                minor_pubblicazioni(query.split(' ')[1])
-            else:
-                speak("Sorry, I can't answer this question")
-
+    print("Do you want to ask me another question?")
     speak("Do you want to ask me another question?")
-    risp = getAudio()
+    # risp = getAudio()
+    risp = input()
+print('Ok, see you next search! Bye!')
 speak('Ok, see you next search! Bye!')
